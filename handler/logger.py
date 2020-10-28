da9161dec2060e32feb2977933771f94fd6d09d4
bcskda <demulkup@ya.ru>
2020-10-29 00:24:33 +0300

    [handler] better logging

--- handler/logger.py
+++ handler/logger.py
@@ -1,12 +1,13 @@
 import logging
 
 LOGLEVEL = logging.INFO
-FORMAT = "%(asctime)s %(levelname)s:%(name)s %(message)s"
-LOGGER_NAME = "EvaCockpit"
-IS_INIT = False
+LOGGER_PREFIX = "EvaCockpit"
+FORMAT = "%(asctime)s %(levelname)s:{}.%(name)s %(message)s" \
+    .format(LOGGER_PREFIX)
 
-def init_logger():
-    logger = logging.getLogger(LOGGER_NAME)
+g_loggers = dict()
+
+def init_logger(logger: logging.Logger):
     handler = logging.StreamHandler()
     formatter = logging.Formatter(FORMAT)
     handler.setFormatter(formatter)
@@ -15,9 +16,11 @@ def init_logger():
     logger.setLevel(LOGLEVEL)
     logger.info("Logs start")
 
-def get_logger():
-    global IS_INIT
-    if not IS_INIT:
-        init_logger()
-        IS_INIT = True
-    return logging.getLogger(LOGGER_NAME)
+def get_logger(name: str):
+    global g_loggers
+    actual_name = "EvaCockpit"
+    if not str in g_loggers:
+        logger = logging.getLogger(name)
+        init_logger(logger)
+        g_loggers[name] = logger
+    return logging.getLogger(name)
